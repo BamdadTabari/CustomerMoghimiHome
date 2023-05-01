4a404ad9db52aa5eec24c26ad7253c07be9f28f3
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-01 01:04:00 -0700

    .

--- CustomerMoghimiHome/Client/Program.cs
+++ CustomerMoghimiHome/Client/Program.cs
@@ -11,11 +11,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IHttpService, HttpService>();
 
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddAuthorizationCore();
-builder.Services.AddBlazoredSessionStorage();
-builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+//builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+//builder.Services.AddBlazoredLocalStorage();
+//builder.Services.AddAuthorizationCore();
+//builder.Services.AddBlazoredSessionStorage();
+//builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 
 builder.Services.AddMudServices(config =>
 {
