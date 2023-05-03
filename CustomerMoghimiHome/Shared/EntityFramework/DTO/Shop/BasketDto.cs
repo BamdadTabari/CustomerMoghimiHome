91aae0fe561326c978a61eac597c9395a3b36d36
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-03 09:53:22 -0700

    dto

--- CustomerMoghimiHome/Shared/EntityFramework/DTO/Shop/BasketDto.cs
+++ CustomerMoghimiHome/Shared/EntityFramework/DTO/Shop/BasketDto.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using CustomerMoghimiHome.Shared.EntityFramework.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomerMoghimiHome.Shared.EntityFramework.DTO.Shop;
-public class BasketDto
+public class BasketDto : BaseDto
 {
     public Guid UserId { get; set; }
     public bool IsDone { get; set; }
