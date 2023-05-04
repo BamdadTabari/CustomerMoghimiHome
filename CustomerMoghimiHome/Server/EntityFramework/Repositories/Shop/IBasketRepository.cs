99659b6822fb779367f2dc8eaa060cf1dd260def
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-05-04 01:58:18 -0700

    add repoes

--- CustomerMoghimiHome/Server/EntityFramework/Repositories/Shop/IBasketRepository.cs
+++ CustomerMoghimiHome/Server/EntityFramework/Repositories/Shop/IBasketRepository.cs
@@ -1,8 +1,25 @@
 ﻿using CustomerMoghimiHome.Server.EntityFramework.Common;
 using CustomerMoghimiHome.Server.EntityFramework.Entities.Shop;
+using CustomerMoghimiHome.Server.EntityFramework.Repositories.Shop;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomerMoghimiHome.Server.EntityFramework.Repositories.Shop;
 
-public interface IBasketRepository : IRepository<BasketEntity>
+public interface IBasketRepository :IRepository<BasketEntity>
 {
+    Task<BasketEntity> GetByIdAsync(long id);
+}
+
+
+public class BasketRepository : Repository<BasketEntity>, IBasketRepository
+{
+    private readonly IQueryable<BasketEntity> _queryable;
+
+    public BasketRepository(DataContext context) : base(context)
+    {
+        _queryable = DbContext.Set<BasketEntity>();
+    }
+
+    public async Task<BasketEntity> GetByIdAsync(long id) =>
+         await _queryable.SingleOrDefaultAsync(x => x.Id == id) ?? throw new NullReferenceException();
 }
